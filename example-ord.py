91e6c24eafd59c10146ef369f5e0c1ca12ce4b34
BCooper58 <brc51101@gmail.com>
2019-01-14 09:27:01 -0800

    kek

---
+++ example-ord.py
@@ -0,0 +1,8 @@
+def main():
+	c = [' ', 'A', 'a', '*', '0']
+	
+	for n in range(len(c)):
+		print(n,c[n],ord(c[n]))
+		
+		
+main()
---
+++ lambda1.py
@@ -0,0 +1,3 @@
+x = lambda a, b, c : a+b+c
+fx = x(5,6,2)
+print(x)
